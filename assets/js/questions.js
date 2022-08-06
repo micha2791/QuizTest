da7a2edba4b8db211b01263722f1e8cc9087bd38
Michael Correa <michaelcorrea@MacBook-Pro-2.attlocal.net>
2022-08-06 03:16:07 -0400

    first commit

---
+++ assets/js/questions.js
@@ -0,0 +1,113 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "Javascript is an _______ language?",
+    answer: "Object Oriented",
+    options: [
+      "Object-Based",
+      "Object Oriented",
+      "Procedural",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following keywords is used to define a variable in Javascript?",
+    answer: "Both A and B",
+    options: [
+      "var",
+      "let",
+      "Both A and B",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following methods is used to access HTML elements using Javascript??",
+    answer: "Both A and B",
+    options: [
+      "getElementById()",
+      "getElementByClassName()",
+      "Both A and B",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Upon encountering empty statements, what does the Javascript Interpreter do?",
+    answer: "Ignores the statement",
+    options: [
+      "Ignores the statement",
+      "Throws an error",
+      "Executes the statement",
+      "Gives a warning"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following methods can be used to display data in some form using Javascript?",
+    answer: "All of the above",
+    options: [
+      "document.write()",
+      "console.log()",
+      "window.alert()",
+      "All of the above"
+    ]
+  },
+    {
+  numb: 6,
+  question: "How can a datatype be declared to be a constant type?",
+  answer: "const",
+  options: [
+    "const",
+    "let",
+    "var",
+    "constant"
+  ]
+},
+{
+  numb: 7,
+  question: "When the switch statement matches the expression with the given labels, how is the comparison done?",
+  answer: "Both the datatype and the result of the expression are compared", 
+  options: [
+    "Both the datatype and the result of the expression are compared",
+    "Only the datatype of the expression is compared",
+    "Only the value of the expression is compared",
+    "None of the above"
+  ]
+},
+{
+  numb: 8,
+  question: "When an operator’s value is NULL, the typeof returned by the unary operator is:",
+  answer: "Object",
+  options: [
+  "Object",
+  "Boolean",
+  "Integer",
+  "Undefined",
+]
+}, 
+{
+  numb: 9,
+  question: "What does the Javascript “debugger” statement do?",
+  answer: "It acts as a breakpoint in a program",
+  options: [
+  "It acts as a breakpoint in a program",
+  "It stops the execution of a program",
+  "It will debug all the error in the program at runtime",
+  "All of the above"
+]
+},
+{
+  numb: 10,
+  question: "Which of the following are closures in Javascript?",
+  answer: "All of the above",
+  options: [
+  "All of the above",
+  "Vaiables",
+  "Functions",
+  "Objects"
+]
+},
+]
